affcf28b53bf16b1a91298a26885b5d538f6149b
Irving Lucas da Silva Melo <irving.lucas@gmail.com>
2022-08-19 18:02:19 -0300

    Added some code to play with the button and the text view.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.button)
+        val resultTextView = findViewById<TextView>(R.id.textView)
+        rollButton.setOnClickListener {
+            resultTextView.text = Random.nextInt(1,6).toString()
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+
     }
 }
