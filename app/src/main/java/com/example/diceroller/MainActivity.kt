de254362183ea37bc0efc31f7e56dd7d246f124a
Irving Lucas da Silva Melo <irving.lucas@gmail.com>
2022-08-21 16:39:23 -0300

    Finished the practice step by adding a second dice and rolling them separately. TODO: check random seed.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,29 +6,42 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 // since we have only 6 drawables of a dice, I decided to fix the value here
-const val DICE_SIDES = 6
+//const val DICE_SIDES = 6
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val diceImageView1 = findViewById<ImageView>(R.id.diceImageView)
+        val diceImageView2 = findViewById<ImageView>(R.id.diceImageView2)
+        // starts the activity with a random dice side
+
+        /*
+        * IDK why, but every time the app starts,
+        * the dice is starting always in a fixed side
+        * but the code is supposed to randomize it.
+        * */
+
+        // check random seed kotlin
+        rollDice(diceImageView1)
+        rollDice(diceImageView2)
+
         val rollButton = findViewById<Button>(R.id.button)
 
-        // starts the activity with a random dice side
-        rollDice()
-
         rollButton.setOnClickListener {
-            rollDice()
+            rollDice(diceImageView1)
+            rollDice(diceImageView2)
         }
-
     }
 
-    private fun rollDice() {
-        val dice = Dice(DICE_SIDES)
-        val diceImage = findViewById<ImageView>(R.id.diceImageView)
+    private fun rollDice(diceImageView: ImageView) {
+        val dice = Dice(6)
 
         val diceRoll = dice.roll()
+
+        //val diceImage = findViewById<ImageView>(R.id.diceImageView)
 
         val diceSide = when (diceRoll) {
             1 -> R.drawable.dice_1
@@ -40,9 +53,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         // update the right image after rolling the dice
-        diceImage.setImageResource(diceSide)
+        diceImageView.setImageResource(diceSide)
         // set the content description for the TalkBack utility
-        diceImage.contentDescription = diceRoll.toString()
+        diceImageView.contentDescription = diceRoll.toString()
     }
-
 }
